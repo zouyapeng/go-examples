31089530638e725643261792deb2ee0ad1174987
邹亚鹏 <yapeng.zou@ymm56.com>
2020-01-01 14:39:42 +0800

    Mutex.go

--- Mutex/Mutex.go
+++ Mutex/Mutex.go
@@ -9,28 +9,29 @@ import (
 // 读写锁，可以有多个读
 var RWLock = sync.RWMutex{}
 
-// 全局锁
+// 互斥锁
 var Lock = sync.Mutex{}
 var wg = sync.WaitGroup{}
 
 func readTask(number int) {
 	// 读锁可以有多个读（CPU数）
+	defer wg.Done()
 	RWLock.RLock()
 	defer RWLock.RUnlock()
 	fmt.Printf("[%d]Start Read.\n", number)
 	time.Sleep(5 * time.Second)
 	fmt.Printf("[%d]End Read.\n", number)
-	wg.Done()
 }
 
 func task(number int) {
 	// 只能一个任务在执行
+	defer wg.Done()
 	Lock.Lock()
 	defer Lock.Unlock()
 	fmt.Printf("[%d]Start.\n", number)
 	time.Sleep(5 * time.Second)
 	fmt.Printf("[%d]End.\n", number)
-	wg.Done()
+
 }
 
 func main() {
